c7edf885f42bb060214207ad9b8e80653a695808
Elis Mora <moralmedina8@gmail.com>
2024-02-07 17:44:19 +0100

    configuracion de webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,63 @@
+const { default: HtmlWebpackPlugin } = require('html-webpack-plugin')
+const { default: MiniCssExtractPlugin } = require('mini-css-extract-plugin')
+const path = require('path')
+
+module.exports = (_, argv) => ({
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    experiments: {
+        outputModule: true
+    },
+    devServer: {
+        port: 3000,
+        historyApiFallback: true
+    },
+    devtool: argv.mode === 'development' ? 'source-map' : false,
+    module: {
+        rules: [
+            {
+                test: /\.mjs$/,
+                exclude: /node_modules/,
+                type: 'javascript/auto',
+                resolve: {
+                    fullySpecified: false
+                }
+            },
+            {
+                test: /\.(ts|tsx|js|mjs|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.css|s[ac]ss$/i,
+                exclude: /node_modules/,
+                use: [argv.mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader, 'sass-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|eot|ttf|woff|woff2|mp3|mp4|wav|webm)$/i,
+                exclude: /node_modules/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/[name][ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            scriptLoading: 'module'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
+})
